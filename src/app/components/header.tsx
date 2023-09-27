72751ed1ca07544ccd4c565d12bb2b53f3bd3468
jairdevt <merajair17@gmail.com>
2023-09-27 14:59:45 -0500

    b

---
+++ src/app/components/header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="bg-red-400 text-center w-full">
+      <h1 className="text-lg font-bold">Tip App</h1>
+    </header>
+  );
+};
+
+export default Header;
